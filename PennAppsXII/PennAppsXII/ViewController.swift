db7614a44d05bc9719302465ce9a8d068d133dbe
Yunchen Wei <weiyunchen0807@outlook.com>
2015-09-04 21:08:44 -0400

    First commit

---
+++ PennAppsXII/PennAppsXII/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PennAppsXII
+//
+//  Created by WEI, Yunchen on 9/4/15.
+//  Copyright © 2015 WEI, Yunchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
